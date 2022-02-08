8c09c86547eef1be11f346a6724eef26398adbed
weichangk <18276743761@163.com>
2022-02-09 00:02:59 +0800

    update

--- MiniShop.Mvc/Code/SetAccessTokenAttribute.cs
+++ MiniShop.Mvc/Code/SetAccessTokenAttribute.cs
@@ -5,7 +5,7 @@ namespace MiniShop.Mvc.Code{    public class SetAccessTokenAttribute : ApiActi
         public override Task OnRequestAsync(ApiRequestContext context)
         {
             var userInfo = (IUserInfo)context.HttpContext.ServiceProvider.GetService(typeof(IUserInfo));
-            context.HttpContext.HttpClient.DefaultRequestHeaders.Add(System.Net.HttpRequestHeader.Authorization.ToString(), $"Bearer {userInfo.AccessToken}");
+            context.HttpContext.HttpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", userInfo.AccessToken);
             return Task.CompletedTask;
         }
     }}
